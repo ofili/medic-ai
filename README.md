f583b7e78b5e0ad28cc56de109f4fce6f44520e6
Lewis <ofili@users.noreply.github.com>
2020-06-21 08:23:56 +0100

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # medic-ai
+Get information on the corona virus
+Main app: https://angry-mahavira-88f2e3.netlify.app/
--- main.js
+++ main.js
@@ -30,23 +30,28 @@ const trigger = [
 // Bot response/trigger
 const reply = [
     [
-        "Hello, I'm Lewis! To start enter \'help\'",
-        "Hi, I'm Lewis! To start enter \'help\'",
-        "Hey, I'm Lewis! To start enter \'help\'",
-        "Hi there, I'm Lewis! To start enter \'help\'",
-        "Good Afternoon, I'm Lewis! To start enter \'help\'"
+        "Hello, I'm Medic AI! To start enter \'help\'",
+        "Hi, I'm Medic AI! To start enter \'help\'",
+        "Hey, I'm Medic AI! To start enter \'help\'",
+        "Hi there, I'm Medic AI! To start enter \'help\'",
+        "Good Afternoon, I'm Medic AI! To start enter \'help\'"
     ],
     [`<br>
-    press \'1\' for tips on how to prevent coronavirus <br>
-    press \'2\' for coronavirus symptoms. <br>
-    press \'3\' to get source of accurate information. <br>
-    press \'4\' to get the number of confirmed cases in Nigeria. <br>`],
+    Enter \'1\' for tips on how to prevent coronavirus <br>
+    Enter \'2\' for coronavirus symptoms. <br>
+    Enter \'3\' to get source of accurate information. <br>
+    Enter \'4\' to get the number of confirmed cases in Nigeria. <br>
+    Enter \'5\' to know how to get tested`],
 
     [
-        `1. Wash your hands frequently with soap and running water. 2. Clean your hand with alcohol-based hand sanitizer for at least 20 second. 3. Wear face mask when you are in a public place. 4. Observe physical distance`,
+        `
+            1. Wash your hands frequently with soap and running water .2.Clean your hand with alcohol - based hand sanitizer
+            for at least 20 second .3.Wear face mask when you are in a public place .4.Observe physical distance `,
         "Fever, chest pain, loss of sense of smell, loss of taste, sore throat, cough, chills, shortness of breath",
         "Always look outfor publications from the Nigeria Center for Disease Control (NCDC)",
-        "Nigeria has recorded 19,808 confirmed cases as at June 2th, 2020."
+        "Nigeria has recorded 19,808 confirmed cases as at June 2th, 2020.",
+        "Call the NCDC toll-free numbers: 0800-970000-10 or send an SMS to: 08099555577 or chat with a rep on WhatsApp: 07087110839"
+
     ]
 ];
 
@@ -114,12 +119,12 @@ function addChat(input, product) {
     const mainDiv = document.getElementById("container");
     let userDiv = document.createElement("div");
     userDiv.id = "user";
-    userDiv.innerHTML = `<strong style="color:blue;">You: </strong><span id="user-response">${input}</span>`;
+    userDiv.innerHTML = `<strong style = "color:blue;">You: </strong><span id="user-response">${input}</span> `;
     mainDiv.appendChild(userDiv);
 
     let botDiv = document.createElement("div");
     botDiv.id = "bot";
-    botDiv.innerHTML = `<strong style="color:red;">Bot: </strong><span id="bot-response">${product}</span>`;
+    botDiv.innerHTML = ` <strong style = "color:red;"> Bot: </strong><span id="bot-response">${product}</span > `;
     mainDiv.appendChild(botDiv);
     // speak(product);
 }
